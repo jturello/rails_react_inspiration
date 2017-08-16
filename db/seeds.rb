e62debaf58093ee81fbe77ce71774fb1588a55d7
James Turello <jturello@yahoo.com>
2017-08-16 13:00:25 -0700

    add seed data for quotes table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quote.delete_all
+
+Quote.create! (
+  [
+    {
+      text: "I love and accept myself unconditionally!",
+      author: "Psychology Today"
+    },
+    {
+      text: "My high self esteem enables me to respect others and beget respect in return!",
+      author: "Psychology Today"
+    },
+    {
+      text: "I am never alone. The universe supports me and is always with me!",
+      author: "Psychology Today"
+    },
+    {
+      text: "I radiate love and respect and receive love and respect in return!",
+      author: "Psychology Today"
+    },
+    {
+      text: "Be yourself; everyone else is alreacy taken.",
+      author: "Oscar Wilde"
+    },
+    {
+      text: "Two things are infinite: the universe and human stupidity; " \
+            "and I'm not sure about the universe.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "Be the change that you wish to see in the world",
+      author: "Mahatma Gandhi"
+    }
+  ]
+)
+puts "Quotes seeded!"
